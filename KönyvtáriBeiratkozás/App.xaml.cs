a8c67777e734af3ec8c53fe782d5a075d2a948fb
Gergo2005 <gatigege05@gmail.com>
2025-11-25 13:37:18 +0100

    Add project files.

---
+++ KönyvtáriBeiratkozás/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KönyvtáriBeiratkozás
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ KönyvtáriBeiratkozás/Olvaso.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Konyvtar
+{
+	public class Olvaso
+	{
+		// Alapértékek
+		private string nev = "";
+		private int eletkor = 0;
+		private string mufaj = "";
+		private bool hirlevel = false;
+		private bool sms = false;
+		private string tagsag = "";
+
+		// Tulajdonságok
+		public string Nev { get => nev; set => nev = value; }
+		public int Eletkor { get => eletkor; set => eletkor = value; }
+		public string Mufaj { get => mufaj; set => mufaj = value; }
+		public bool Hirlevel { get => hirlevel; set => hirlevel = value; }
+		public bool SmsErtesites { get => sms; set => sms = value; }
+		public string Tagsag { get => tagsag; set => tagsag = value; }
+
+		// ToString → fájlba írás formátuma
+		public override string ToString()
+		{
+			return $"{Nev}|{Eletkor}|{Mufaj}|{Hirlevel}|{SmsErtesites}|{Tagsag}";
+		}
+
+		// FromString → fájlból visszaalakítás
+		public static Olvaso FromString(string sor)
+		{
+			var adat = sor.Split('|');
+
+			return new Olvaso
+			{
+				Nev = adat[0],
+				Eletkor = int.Parse(adat[1]),
+				Mufaj = adat[2],
+				Hirlevel = bool.Parse(adat[3]),
+				SmsErtesites = bool.Parse(adat[4]),
+				Tagsag = adat[5]
+			};
+		}
+	}
+}
